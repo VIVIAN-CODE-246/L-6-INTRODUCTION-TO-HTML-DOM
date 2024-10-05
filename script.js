46fdaef993d63a17daff1ff48e640d5b73ac6f23
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2024-10-05 13:55:36 +0300

    DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+function pen(){
+    document.getElementById("VIVIAN'S WEBSITE").innerHTML = "MY FIRST WEBSITE"
+}
+document.body.style.backgroundColor = "black"
+document.getElementById("VIVIAN'S WEBSITE").style.color = "white"
